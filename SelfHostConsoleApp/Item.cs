2b7ce5e4ceeee2323cad499b09d3faf07b1abc7e
Julio Cesar Yepes Ruiz <julio.yepes@3ysolutions.com.co>
2021-08-20 10:27:11 -0500

    Add project files.

---
+++ SelfHostConsoleApp/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace SelfHostConsoleApp
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+    }
+}
