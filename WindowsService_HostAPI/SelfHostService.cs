91d1bbadb9d2b8fe90547bf2aee049b8bf2fc020
Julio Cesar Yepes Ruiz <julio.yepes@3ysolutions.com.co>
2021-08-23 10:11:34 -0500

    Hosting in windows service

---
+++ WindowsService_HostAPI/SelfHostService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace WindowsService_HostAPI
+{
+    public partial class SelfHostService : ServiceBase
+    {
+        public SelfHostService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //configuración del Api y servidor HTTP
+            var config = new HttpSelfHostConfiguration("http://localhost:8080");
+            config.Routes.MapHttpRoute(
+                name:"API",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+            
+            using (HttpSelfHostServer server = new HttpSelfHostServer(config))
+            {
+                server.OpenAsync().Wait();
+            }
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ WindowsService_HostAPI/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WindowsService_HostAPI
+{
+    public class ValuesController: ApiController
+    {
+
+        public string GetString(Int32 id)
+        {
+            return "Este es un string retornado a través de un Windows Service";
+        }
+    }
+}
